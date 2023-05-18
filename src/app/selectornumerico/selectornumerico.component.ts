627ce7523d946bfa9844423d5ec56d7372008c66
gianA15 <gian.arce@tecsup.edu.pe>
2023-05-17 20:50:21 -0500

    primer commit

---
+++ src/app/selectornumerico/selectornumerico.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-selectornumerico',
+  templateUrl: './selectornumerico.component.html',
+  styleUrls: ['./selectornumerico.component.css']
+})
+
+export class SelectornumericoComponent {
+  @Input() minimo: number = 0;
+  @Input() maximo: number = 0;
+
+  actual: number = 0;
+
+  constructor() {
+   
+  }
+
+  incrementar() {
+    if (this.actual < this.maximo) {
+      this.actual++;
+    }
+  }
+
+  decrementar() {
+    if (this.actual > this.minimo) {
+      this.actual--;
+    }
+  }
+
+  fijar(v: number) {
+    if (v >= this.minimo && v <= this.maximo) {
+      this.actual = v;
+    }
+  }
+}
+
